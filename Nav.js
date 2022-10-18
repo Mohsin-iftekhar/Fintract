a406dd44a79e319f38696e42641e105665604464
Mohsin-iftekhar <85876751+Mohsin-iftekhar@users.noreply.github.com>
2022-10-18 20:40:32 +0530

    mychange

--- Nav.js
+++ Nav.js
@@ -1,98 +1,56 @@
-import React, { useState } from 'react';
-import {
-  MDBContainer,
-  MDBNavbar,
-  MDBNavbarBrand,
-  MDBNavbarToggler,
-  MDBIcon,
-  MDBNavbarNav,
-  MDBNavbarItem,
-  MDBNavbarLink,
-  MDBBtn,
-  MDBDropdown,
-  MDBDropdownToggle,
-  MDBDropdownMenu,
-  MDBDropdownItem,
-  MDBCollapse,
-} from 'mdb-react-ui-kit';
-import Image from 'next/image';
-import styles from '/styles/Home.module.css'
-export default function Nav() {
-  const [showBasic, setShowBasic] = useState(false);
+import Link from "next/link";
+import Image from "next/image";
+import React, { useState } from "react";
+import NavItem from "./NavItem"; 
+
+const MENU_LIST = [
+  { text: "Home", href: "/" ,className:"logo"},
+  { text: "About Us", href: "/about",className:"logo" },
+  { text: "Contact", href: "/contact",className:"logo" },
+];
+const Navbar = () => {
+  const [navActive, setNavActive] = useState(null);
+  const [activeIdx, setActiveIdx] = useState(-1);
 
   return (
-    <MDBNavbar expand='lg' light bgColor='light'>
-      <MDBContainer fluid>
-        <Image 
-        src="/finlogo.png"
-        height={80}
-        width={50}
-        />
-2
-        <MDBNavbarToggler
-          aria-controls='navbarSupportedContent'
-          aria-expanded='false'
-          aria-label='Toggle navigation'
-          onClick={() => setShowBasic(!showBasic)}
+    <header>
+      <nav className={`nav`}>
+      <Image 
+      src="/finlogo.png"
+      height={80}
+      width={50}
+      />
+        <Link href={"/"}>
+      
+          <a>
+            <h1 className="logo">Fintract</h1>
+
+          </a>
+        </Link>
+        <div
+          onClick={() => setNavActive(!navActive)}
+          className={`nav__menu-bar`}
         >
-          <MDBIcon icon='bars' fas />
-        </MDBNavbarToggler>
-
-        <MDBCollapse navbar show={showBasic} className='justify-content-center'>
-          <MDBNavbarNav className='mr-auto mb-2 mb-lg-0 mx-5 justify-content-center'>
-            <MDBNavbarItem>
-              <MDBNavbarLink active aria-current='page' href='#'>
-                Contact Us
-              </MDBNavbarLink>
-            </MDBNavbarItem>
-            <MDBNavbarItem>
-              <MDBNavbarLink href='#'>About Us</MDBNavbarLink>
-            </MDBNavbarItem>
-
-            <MDBNavbarItem>
-              <MDBDropdown>
-                <MDBDropdownToggle tag='a' className='nav-link'>
-                  Our Products
-                </MDBDropdownToggle>
-                <MDBDropdownMenu>
-                  <MDBDropdownItem link>Action</MDBDropdownItem>
-                  <MDBDropdownItem link>Another action</MDBDropdownItem>
-                  <MDBDropdownItem link>Something else here</MDBDropdownItem>
-                </MDBDropdownMenu>
-              </MDBDropdown>
-            </MDBNavbarItem>
-            <MDBNavbarItem>
-            <MDBDropdown>
-              <MDBDropdownToggle tag='a' className='nav-link'>
-              More
-              </MDBDropdownToggle>
-              <MDBDropdownMenu>
-                <MDBDropdownItem link>Action</MDBDropdownItem>
-                <MDBDropdownItem link>Another action</MDBDropdownItem>
-                <MDBDropdownItem link>Something else here</MDBDropdownItem>
-              </MDBDropdownMenu>
-            </MDBDropdown>
-          </MDBNavbarItem>
-          </MDBNavbarNav>
-          <form className='d-flex input-group w-auto'>
-          <MDBBtn outline color='light' className={styles.login}>
-         <h3>LogIn
-         <Image src="/login.png"
-          // className={styles.ii}
-          height={28}
-          width={28}
-          /></h3>
-         
-        </MDBBtn>
-       
-          </form>
-          <Image
-          src="/sear.png"
-          height={40}
-          width={40}
-          />
-        </MDBCollapse>
-      </MDBContainer>
-    </MDBNavbar>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+        <div className={`${navActive ? "active" : ""} nav__menu-list`}>
+          {MENU_LIST.map((menu, idx) => (
+            <div
+              onClick={() => {
+                setActiveIdx(idx);
+                setNavActive(false);
+              }}
+              key={menu.text}
+            >
+              <NavItem active={activeIdx === idx} {...menu} />
+            </div>
+          ))}
+        </div>
+      </nav>
+    </header>
   );
-}
+};
+
+export default Navbar;
