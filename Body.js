d3397c1a521c409a85a3b21fc45a31590e9746ca
Mohsin-iftekhar <85876751+Mohsin-iftekhar@users.noreply.github.com>
2022-10-18 21:00:55 +0530

    mychangeesss

--- Body.js
+++ Body.js
@@ -4,22 +4,7 @@ import styles from '/styles/Home.module.css'
 function Body() {
   return (
     <div>
-    <section className={styles.background+" "+styles.firstsec}>
-    <div className={styles.boxmain}>
-        <div className={styles.firsthalf}>
-  <p className={styles.textbig}>FINTRACT GLOBAL</p>
-       <p className={styles.textsmall}>
-       Fintract Global takes your privacy very seriously.<br/>
-        We may process your personal information for carefully<br/>
-         considered and specific purposes which are in our interests<br/>
-          and enable us to enhance the  services we provide. 
-       </p>
-      
-        </div>
-      <div className={styles.get} > Get started</div>
-      <div className={styles.knowmore}>Know More</div>
-    </div>
-</section>
+   
     </div>
   )
 }
